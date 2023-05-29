7ce3c12646d7e6ea60df2aacf0448dfe44d75c9e
ManoharDarla2 <112201034@smail.iitpkd.ac.in>
2023-05-29 13:57:12 +0530

    added stand images script and output images

---
+++ assets/scripts/stand_images.py
@@ -0,0 +1,5 @@
+from run_images import scale_path, MARIO_PATH
+from utils.utils import image_scale
+
+image_scale(f'{scale_path}/stand/stan1.png', f'{MARIO_PATH}/stand/stand1.png', scale=0.2)
+image_scale(f'{scale_path}/stand/stan2.png', f'{MARIO_PATH}/stand/stand2.png', scale=0.2)

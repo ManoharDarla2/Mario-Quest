43cd87a86dae5c459149db52541959267b55ed4f
ManoharDarla2 <112201034@smail.iitpkd.ac.in>
2023-05-29 12:26:29 +0530

    Update PATHS in path.py

--- utils/paths.py
+++ utils/paths.py
@@ -1,2 +1,4 @@
-MENU_PATH = 'C:/Users/Administrator/PycharmProjects/Mario-Quest/assets/images/menu'
-MAIN_PATH = 'C:/Users/Administrator/PycharmProjects/Mario-Quest/assets/images/main'
+ASSETS_PATH = 'C:/Users/Administrator/PycharmProjects/Mario-Quest/assets'
+MENU_PATH = f'{ASSETS_PATH}/images/menu'
+MAIN_PATH = f'{ASSETS_PATH}/images/main'
+MARIO_PATH = f'{ASSETS_PATH}/images/mario'
